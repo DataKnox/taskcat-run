66c5f6416f1d8829494540590004d4039b77f4ab
Knox Hutchinson <knox@knoxsdata.com>
2023-03-27 10:02:39 -0500

    fix s3 bucket

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-aws cloudformation create-stack --stack-name pipeline-taskcat --capabilities CAPABILITY_NAMED_IAM --disable-rollback --template-body file:////Users/knox/Code/awsSamples/dvac02/iac_taskcat/cf_temp.yml --parameters ParameterKey=GitHubUser,ParameterValue=DataKnox ParameterKey=GitHubRepo,ParameterValue=aws-iac
+aws cloudformation create-stack --stack-name taskcat-stack --capabilities CAPABILITY_NAMED_IAM --disable-rollback --template-body file:////Users/knox/Code/taskcat-run/cf_temp.yml --parameters ParameterKey=GitHubUser,ParameterValue=DataKnox ParameterKey=GitHubRepo,ParameterValue=taskcat-run
 
 # Sample pipeline https://gist.github.com/kevinkarwaski/00aa01826d88650ae5c0da54ae93f258
